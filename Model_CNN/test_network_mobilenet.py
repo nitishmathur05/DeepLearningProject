c02884f4a63edd7b5051e8b289a38d462faf246e
nitishmathur92 <nitish@detexian.com>
2019-08-11 21:32:51 +1000

    Test script changes

--- Model_CNN/test_network_mobilenet.py
+++ Model_CNN/test_network_mobilenet.py
@@ -1,95 +1,127 @@
-
-#   Imports
+import os
 import tensorflow as tf
 import numpy as np
-import argparse
+import cv2
 
-# Paths to files producted as part of retraining Inception.  Change these if you saved your files in
-#   a different location.
-#   Retrained graph
+# Variables --------------------------------------------------------------------------------------------
+# This section is for mobilenet
+MODEL_NAME = 'mobilenet_1.0_224'
+IMAGE_ENTRY = 'DecodeJpgInput:0'
 
-MODEL_PATH = "/tmp/output_graph_mobilenet_run_1.pb"
+RETRAINED_LABELS_TXT_FILE_LOC = "/tmp/output_labels_mobilenet_run_1.txt"
+RETRAINED_GRAPH_PB_FILE_LOC = "/tmp/output_graph_mobilenet_run_1.pb"
 
-LABEL_PATH = "/tmp/output_labels_mobilenet_run_1.txt"
+TEST_IMAGES_DIR = "/mnt/project/NPDI/test_images"
+NON_PORN_DIR = TEST_IMAGES_DIR + '/non_porn'
+PORN_DIR = TEST_IMAGES_DIR + '/porn'
 
+SCALAR_RED = (0.0, 0.0, 255.0)
+SCALAR_BLUE = (255.0, 0.0, 0.0)
+# ---------------------------------------------------------------------------------------------------------
 
-#   Remove ugly characters from strings
-def filter_delimiters(text):
-    filtered = text[:-3]
-    filtered = filtered.strip("b'")
-    filtered = filtered.strip("'")
-    return filtered
+def main():
 
+    # Read classification from label file
+    classifications = []
+    for currentLine in tf.gfile.GFile(RETRAINED_LABELS_TXT_FILE_LOC):
+        classification = currentLine.rstrip()
+        classifications.append(classification)
 
-def predict_image_class(imagePath, labelPath):
-    
-    matches = None # Default return to none
+    # load the graph from file
+    with tf.gfile.FastGFile(RETRAINED_GRAPH_PB_FILE_LOC, 'rb') as retrainedGraphFile:
+        # instantiate a GraphDef object
+        graphDef = tf.GraphDef()
+        # read in retrained graph into the GraphDef object
+        graphDef.ParseFromString(retrainedGraphFile.read())
+        # import the graph into the current default Graph, note that we don't need to be concerned with the return value
+        _ = tf.import_graph_def(graphDef, name='')
 
-    if not tf.gfile.Exists(imagePath):
-        tf.logging.fatal('File does not exist %s', imagePath)
-        return matches
-
-    # Load the image from file
-    image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
-
-    # Load the retrained inception based graph
-    with tf.gfile.FastGFile(MODEL_PATH, 'rb') as f:
-            # init GraphDef object
-            graph_def = tf.GraphDef()
-            # Read in the graphy from the file
-            graph_def.ParseFromString(f.read())
-            _ = tf.import_graph_def(graph_def, name='')
-        # this point the retrained graph is the default graph
+    # if the test image directory listed above is not valid, show an error message and bail
+    if not os.path.isdir(NON_PORN_DIR):
+        print("Directory ", NON_PORN_DIR, ' doesnt exist')
+        return
+    if not os.path.isdir(PORN_DIR):
+        print("Directory ", PORN_DIR, ' doesnt exist')
+        return
+    # end if
 
     with tf.Session() as sess:
-        # These 2 lines are the code that does the classification of the images 
-        # using the new classes we retrained Inception to recognize. 
-        #   We find the final result tensor by name in the retrained model
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-        #   Get the predictions on our image by add the image data to the tensor
-        predictions = sess.run(softmax_tensor,
-                            {'DecodeJpgInput:0': image_data})
-        
-        # Format predicted classes for display
-        #   use np.squeeze to convert the tensor to a 1-d vector of probability values
-        predictions = np.squeeze(predictions)
 
-        top_k = predictions.argsort()[-5:][::-1]  # Getting the indicies of the top 5 predictions
+        # for each folder in the test images directory . . .
+        for group in next(os.walk(TEST_IMAGES_DIR))[1]:
+            # Get classification group folder dir
+            groupDir = os.path.join(TEST_IMAGES_DIR, group)
 
-        #   read the class labels in from the label file
-        f = open(labelPath, 'rb')
-        lines = f.readlines()
-        labels = [str(w).replace("\n", "") for w in lines]
-        print("")
-        print ("Image Classification Probabilities")
-        #   Output the class probabilites in descending order
-        for node_id in top_k:
-            human_string = filter_delimiters(labels[node_id])
-            score = predictions[node_id]
-            print('{0:s} (score = {1:.5f})'.format(human_string, score))
+            # for each sub-folder in each classification folder
+            for subgroup in next(os.walk(groupDir))[1]:
 
-        print("")
+                # Get subgroup folder dir
+                subgroupDir = os.path.join(groupDir, subgroup)
 
-        answer = labels[top_k[0]]
-        return answer
+                # Create result log for this subgroup of test images
+                resultLog = open(subgroupDir + '_' + MODEL_NAME + '.txt', 'w')
 
-# Get the path to the image you want to predict.  
-if __name__ == '__main__':
-    
-    # Ensure the user passes the image_path
-    parser = argparse.ArgumentParser(description="Process arguments")
-    parser.add_argument(
-      'image_path',
-      type=str,
-      default='',
-      help='Path of image to classify.'
-    )
-    args = parser.parse_args()
+                # For each image file inside the sub-group folder
+                for fileName in os.listdir(subgroupDir):
 
-    # We can only handle jpeg images.   
-    if args.image_path.lower().endswith(('.jpg', '.jpeg')):
-        # predict the class of the image
-        predict_image_class(args.image_path, LABEL_PATH)
-    else:
-        print('File must be a jpeg image.')
-    
+                    # if the file does not end in .jpg or .jpeg (case-insensitive), continue with the next iteration of the for loop
+                    if not (fileName.lower().endswith(".jpg") or fileName.lower().endswith(".jpeg")):
+                        continue
+                    # show the file name on std out
+                    print(fileName)
+
+                    imageFileWithPath = os.path.join(subgroupDir, fileName)
+                    # attempt to open the image with OpenCV
+                    openCVImage = cv2.imread(imageFileWithPath)
+
+                    # get the final tensor from the graph
+                    finalTensor = sess.graph.get_tensor_by_name('final_result:0')
+
+                    # convert the OpenCV image (numpy array) to a TensorFlow image
+                    # Some image may corrupt from google search
+                    try:
+                        tfImage = np.array(openCVImage)[:, :, 0:3]
+                    except IndexError:
+                        continue
+
+                    # run the network to get the predictions
+                    predictions = sess.run(finalTensor, {IMAGE_ENTRY : tfImage})
+
+                    # sort predictions from most confidence to least confidence
+                    sortedPredictions = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+                    print("---------------------------------------")
+
+                    onMostLikelyPrediction = True
+                    # for each prediction . . .
+                    for prediction in sortedPredictions:
+                        strClassification = classifications[prediction]
+
+                        # if the classification (obtained from the directory name) ends with the letter "s", remove the "s" to change from plural to singular
+                        if strClassification.endswith("s"):
+                            strClassification = strClassification[:-1]
+                        # end if
+
+                        # get confidence, then get confidence rounded to 2 places after the decimal
+                        confidence = predictions[0][prediction]
+
+                        # if we're on the first (most likely) prediction, state what the object appears to be and show a % confidence to two decimal places
+                        if onMostLikelyPrediction:
+
+                            # Write result log
+                            resultLog.write(fileName + '\t' + strClassification + '\t' + str(confidence) + '\n')
+
+                            onMostLikelyPrediction = False
+
+                resultLog.close()
+
+
+
+    # write the graph to file so we can view with TensorBoard
+    tfFileWriter = tf.summary.FileWriter(TEST_IMAGES_DIR)
+    tfFileWriter.add_graph(sess.graph)
+    tfFileWriter.close()
+
+
+if __name__ == "__main__":
+    main()
