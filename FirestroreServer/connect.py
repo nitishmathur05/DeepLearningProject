4bc3dffe5672bf877f17a03c4cd08aa2dfa00f91
nitishmathur92 <nitish@detexian.com>
2019-09-01 17:22:04 +1000

    Adding to connect

--- FirestroreServer/connect.py
+++ FirestroreServer/connect.py
@@ -5,3 +5,6 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate('../firebase_key.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+print ("Success!!")
+print (db)
