f30c73687d4bf53c88b6b0726d1b5a0dfc834cc3
nitishmathur92 <nitish@detexian.com>
2019-09-01 17:31:20 +1000

    Adding bucket config

--- FirestroreServer/connect.py
+++ FirestroreServer/connect.py
@@ -2,9 +2,40 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('../firebase_key.json')
-default_app = firebase_admin.initialize_app(cred)
+
+
+
+
+def hello_world():
+	print ("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+firebase_key = '../firebase_key.json'
+bucket_name = 'gs://chatapplication-d7a3e.appspot.com/'
+
+cred = credentials.Certificate(firebase_key)
+
+default_app = firebase_admin.initialize_app(cred, {
+    'storageBucket': bucket_name
+})
+
 db = firestore.client()
 
+bucket = storage.bucket()
 print ("Success!!")
-print (db)
+print (bucket)
