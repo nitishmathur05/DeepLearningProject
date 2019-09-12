8ce636ac23ed1c92a0d229fd30e38e56dd800a62
Nitish Mathur <nitishm@student.unimelb.edu.au>
2019-09-12 18:04:21 +0000

    u1

--- cloud_func/functions/index.js
+++ cloud_func/functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage')();
+const gcs = require('@google-cloud/storage');
 const os = require('os');
 const path = require('path');
 // // Create and Deploy Your First Cloud Functions
