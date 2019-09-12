729ff8447a952e8abee8dcc92b74632dd1679aec
nitishmathur92 <nitish@detexian.com>
2019-09-13 03:53:22 +1000

    Changing name of file

--- cloud_func/functions/index.js
+++ cloud_func/functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
-
+const gcs = require('@google-cloud/storage')();
+const os = require('os');
+const path = require('path');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,8 +10,29 @@ const functions = require('firebase-functions');
 // });
 
 exports.myfirstcouldfunc = functions.storage.object().onFinalize(event => {
+	const object = event.data;
+	const bucket = object.bucket;
+	const contentType = object.contentType;
+	const filePath = object.name;
+	
 	console.log("Hello from nitish");
-	console.log(event);
-	return;
+
+	if (path.basename(filePath).startsWith('sensored-image')) {
+		console.log("File already processed");
+		return;
+	}
+
+	const destBucket = gcs.bucket(bucket);
+	const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+	const metadata = { contentType: contentType };
+
+	return destBucket.file(filePath).download({
+		destination : tmpFilePath
+	}).then( () => {
+		return destBucket.upload(tmpFilePath, {
+			destination : 'sensored-image' + path.basename(filePath),
+			metadata : metadata
+		})
+	});
 });
 
