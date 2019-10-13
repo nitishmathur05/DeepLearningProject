262032fb02666c01c88f94c0498c83ddc3912d91
Nitish Mathur <nitishm@student.unimelb.edu.au>
2019-10-13 14:05:38 +0000

    graphDef

--- Model_CNN/test_network_mobilenet_new.py
+++ Model_CNN/test_network_mobilenet_new.py
@@ -20,7 +20,7 @@ import cv2
 
 # This section is for mobilenet
 MODEL_NAME = 'mobilenet_1.0_224'
-IMAGE_ENTRY = 'DecodeJpgInput:0'
+IMAGE_ENTRY = 'DecodeJpgInput'
 
 RETRAINED_LABELS_TXT_FILE_LOC = "/mnt/project/MobileNet/output_labels_mobilenet_run_oct_13_8K.txt"
 RETRAINED_GRAPH_PB_FILE_LOC = "/mnt/project/MobileNet/output_graph_mobilenet_run_oct_13_8K.pb"
