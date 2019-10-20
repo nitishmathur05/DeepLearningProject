7cb5b0b71cb27e177bf32096bd0a94d196c3a933
nitishmathur92 <nitish@detexian.com>
2019-10-21 00:45:09 +1100

    downgrading version

--- mobilenet_v2/new_train_mobilenet_v2.py
+++ mobilenet_v2/new_train_mobilenet_v2.py
@@ -1323,7 +1323,7 @@ if __name__ == '__main__':
       '--tfhub_module',
       type=str,
       default=(
-          'https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/4'),
+          'https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/3'),
       help="""\
       Which TensorFlow Hub module to use. For more options,
       search https://tfhub.dev for image feature vector modules.\
